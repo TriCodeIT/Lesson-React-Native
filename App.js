218d544813462cbee180ca1948af2e209a62a782
TriCodeIT <bayu165kusuma@gmail.com>
2021-09-02 21:19:17 +0700

    ScrollView

--- App.js
+++ App.js
@@ -1,35 +1,62 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  };
+
   return (
+    <ScrollView>
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="Course Goal"
           style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View></View>
+      <ScrollView>
+        {courseGoals.map(goal => (
+          <View key={goal} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50,
+    padding: 50
   },
   inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
   input: {
-    width: "80%",
-    borderColor: "black",
+    width: '80%',
+    borderColor: 'black',
     borderWidth: 1,
+    padding: 10
+  },
+  listItem: {
     padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
