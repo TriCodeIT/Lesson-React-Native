ab4e59954356a71e0984f4ca8bdea4f450fbb8ff
TriCodeIT <bayu165kusuma@gmail.com>
2021-09-02 16:07:00 +0700

    Flexbox Deep Dive, Inline Styles & Stylesheet Objects

--- App.js
+++ App.js
@@ -1,24 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{padding: 50}} >
-     <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-      <TextInput 
-      placeholder="Course Goal"
-      style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}
-      />
-      <Button title="ADD" />
-     </View>
-     <View>
-
-     </View>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  }
 });
